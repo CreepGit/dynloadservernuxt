c986c231be36711e9ee3b54f371cec585f3fe19e
Creep <54237803+CreepGit@users.noreply.github.com>
2024-08-19 15:11:47 +0300

    Error page displays non 404 errors

--- error.vue
+++ error.vue
@@ -1,10 +1,26 @@
 <template>
-    <Toast position="bottom-right" />
-    <NavBar />
-    <Card>
-        <template #content>
-            <h1>Oh no!</h1>
-            <p>This page does not contain anything!</p>
-        </template>
-    </Card>
+    <div>
+        <Toast position="bottom-right" />
+        <NavBar />
+        <Card>
+            <template #content v-if="error?.statusCode == 404">
+                <h1>Oh no!</h1>
+                <p>This is not a real page!</p>
+            </template>
+            <template #content v-else>
+                <h1>Oh no!</h1>
+                <p>An error occurred!</p>
+                <h2>{{ error?.statusCode }}</h2>
+                <pre>{{ {...error, statuCode: undefined, } }}</pre>
+            </template>
+        </Card>
+    </div>
 </template>
+
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const props = defineProps({
+  error: Object as () => NuxtError
+})
+</script>
